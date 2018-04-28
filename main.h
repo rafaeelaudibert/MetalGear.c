26c4a6f92440d04cfeb0d050af92e0c32cf12801
RafaAudibert <rafaeelaudibert@gmail.com>
2018-04-28 01:27:46 -0300

    Adding the main header

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <Windows.h>
+#include <locale.h>
+#include "constantes.h"
+#include "structs.h"
+#include "conio/conio.h"
+#include "mapa.h"
+
+
