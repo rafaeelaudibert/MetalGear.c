1c9777f798ca5d66ba76f4c396519349b22c0ed7
RafaAudibert <rafaeelaudibert@gmail.com>
2018-04-28 01:27:28 -0300

    Adding structs

---
+++ structs.h
@@ -0,0 +1,47 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+//Heroi principal
+typedef struct {
+    int x;      // Posição x do heroi
+    int y;      // Posição y do heroi
+    int dardos; // Quantidade de dardos disponiveis para o heroi
+    int pontos; // Pontos do heroi
+    int vidas;  // Vida restante pro heroi
+    int ciclos; // Ciclos até se movimentar novamente
+} HEROI;
+
+typedef struct{
+    int x;
+    int y;
+    int t_restante;
+}TIRO;
+
+// Inimigo
+typedef struct {
+    int x;
+    int y;
+    int ciclos; // Ciclos até se movimentar novamente
+    int estado; // 0 - Dormindo; 1 - Patrulhando;
+    int t_sono; // Tempo que falta para dormir
+} INIMIGO;
+
+//Todos os inimigos
+typedef struct {
+    int qtde;
+    INIMIGO listaInimigos[MAX_INIMIGOS];
+} INIMIGOS;
+
+// Informação sobre a saída do mapa
+typedef struct {
+    int x;
+    int y;
+    int ativo; // Só pode sair se a porta estiver ativada, ou seja, quando pegar a chave
+} SAIDA;
+
+typedef struct {
+    int x;
+    int y;
+} CHAVE;
+
+#endif // STRUCTS_H
